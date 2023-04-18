b2df1559f4e285b1dcfa90466ef6219abb4af5ef
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-04-19 00:49:28 +0300

    new-ready-queries

--- src/Gabot.php
+++ src/Gabot.php
@@ -61,18 +61,115 @@ class Gabot extends Client
 
     /**
      * Run requests
+     * @link for more info about create request: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
      */
     public function runRequest(array $request): array
     {
         return $this->report_builder->getReports($this->client->batchRunReports($this->setRequest($request)));
     }
 
-     /**
+    /**
      * Run Realtime requests
+     * @link for more info about create request: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
      */
     public function runRealtimeRequest(Query $request): array
     {
         return $this->report_builder->getReports($this->client->runRealtimeReport($this->setRealtimeRequest($request)));
     }
 
+    /**
+     * Get active users by device
+     * @param string $activity_limit "activeUsers", "active1dayUsers", "active7dayUsers", "active28dayUsers"
+     * @link for more info: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
+     */
+    public function getActiveUsersByDevice(string $start_date = "28daysAgo", string $end_date = "today", string $activity_limit = "activeUsers"): array
+    {
+        return $this->runRequest([
+            new Query(
+                metrics: [$activity_limit],
+                dimensions: ["deviceCategory"],
+                date_ranges: ["start_date" => $start_date, "end_date" => $end_date]
+            )
+        ]);
+    }
+
+    /**
+     * Get active users by os
+     * @param string $activity_limit "activeUsers", "active1dayUsers", "active7dayUsers", "active28dayUsers"
+     * @link for more info: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
+     */
+    public function getActiveUsersByOS(string $start_date = "28daysAgo", string $end_date = "today", string $activity_limit = "activeUsers"): array
+    {
+        return $this->runRequest([
+            new Query(
+                metrics: [$activity_limit],
+                dimensions: ["operatingSystem"],
+                date_ranges: ["start_date" => $start_date, "end_date" => $end_date]
+            )
+        ]);
+    }
+
+    /**
+     * Get active users by browser
+     * @param string $activity_limit "activeUsers", "active1dayUsers", "active7dayUsers", "active28dayUsers"
+     * @link for more info: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
+     */
+    public function getActiveUsersByBrowser(string $start_date = "28daysAgo", string $end_date = "today", string $activity_limit = "activeUsers"): array
+    {
+        return $this->runRequest([
+            new Query(
+                metrics: [$activity_limit],
+                dimensions: ["browser"],
+                date_ranges: ["start_date" => $start_date, "end_date" => $end_date]
+            )
+        ]);
+    }
+
+    /**
+     * Get active users by city
+     * @param string $activity_limit "activeUsers", "active1dayUsers", "active7dayUsers", "active28dayUsers"
+     * @link for more info: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
+     */
+    public function getActiveUsersByCity(string $start_date = "28daysAgo", string $end_date = "today", string $activity_limit = "activeUsers"): array
+    {
+        return $this->runRequest([
+            new Query(
+                metrics: [$activity_limit],
+                dimensions: ["city"],
+                date_ranges: ["start_date" => $start_date, "end_date" => $end_date]
+            )
+        ]);
+    }
+
+    /**
+     * Get active users by country
+     * @param string $activity_limit "activeUsers", "active1dayUsers", "active7dayUsers", "active28dayUsers"
+     * @link for more info: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
+     */
+    public function getActiveUsersByCountry(string $start_date = "28daysAgo", string $end_date = "today", string $activity_limit = "activeUsers"): array
+    {
+        return $this->runRequest([
+            new Query(
+                metrics: [$activity_limit],
+                dimensions: ["country"],
+                date_ranges: ["start_date" => $start_date, "end_date" => $end_date]
+            )
+        ]);
+    }
+
+    /**
+     * Get active users by country and city
+     * @param string $activity_limit "activeUsers", "active1dayUsers", "active7dayUsers", "active28dayUsers"
+     * @link for more info: https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en
+     */
+    public function getActiveUsersByCountryAndCity(string $start_date = "28daysAgo", string $end_date = "today", string $activity_limit = "activeUsers"): array
+    {
+        return $this->runRequest([
+            new Query(
+                metrics: [$activity_limit],
+                dimensions: ["country", "city"],
+                date_ranges: ["start_date" => $start_date, "end_date" => $end_date]
+            )
+        ]);
+    }
 }
